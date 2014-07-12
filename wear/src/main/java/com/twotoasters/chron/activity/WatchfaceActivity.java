217702a5786d7fe7cb8e6a53098a980c887aa13c
jbarr21 <james@twotoasters.com>
2014-07-12 16:53:04 -0400

    Added initial project files

---
+++ wear/src/main/java/com/twotoasters/chron/activity/WatchfaceActivity.java
@@ -0,0 +1,22 @@
+package com.twotoasters.chron.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WatchfaceActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
